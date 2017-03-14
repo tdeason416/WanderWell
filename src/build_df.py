5594eeb369ff7abc9b7a322ccb23121de6cd3a05
tdeason416 <tdeason416@gmail.com>
2017-03-14 08:40:41 -0700

    create_reviews_df function now working

--- src/build_df.py
+++ src/build_df.py
@@ -79,27 +79,119 @@ def create_flattened_dataframe(json_folder):
                 poi_list.append(pd.io.json.json_normalize(bus_json))
     city_df = pd.concat(poi_list)
     for cat in ['category-0', 'category-1', 'category-2'] :
-        city[cat].fillna(False, inplace=True)
+        city_df[cat].fillna(False, inplace=True)
     return city_df
 
-def save_df_to_s3_json(df, bucket_name, file_name):
+# def split_dict_rows(cell, appendee):
+#     '''
+#     Splits cells from list of rows into 
+#     ========
+#     PARAMETERS
+#     cell: cell containing dictionary
+#     appendee: lit to add dictionaries to
+#     =======
+#     RETURNS
+#     None
+#     '''
+#     appendee.append(dict)
+
+def add_list_contents_to_df(row, appendee):
+    '''
+    appends rows in df to new columns
+    ========
+    PARAMETERS
+    row: dataframe row containing list if items
+    apandee: list to add items onto
+    ========
+    RETURNS
+    None
+    '''
+    reviews = {}
+    i= 0
+    for review in row:
+        i += 1
+        reviews['review-{}'.format(i)] = review
+    appendee.append(reviews)
+
+def create_reviews_df(df):
+    '''
+    Generates reviews dataframe from existing object
+    ========
+    PARAMETERS
+    df: pandas df - from create_flattened_dataframe function
+    ========
+    RETURNS
+    review_df: pandas df - dataframe containing reviews
+    '''
+    reviews_1 = df['id']
+    keys = df['reviews'].values[0][0].keys()
+    review_rows = []
+    df['reviews'].apply(add_list_contents_to_df, appendee= review_rows)
+    reviews = pd.DataFrame(review_rows)
+    reviews_ = reviews.set_index(reviews_1)
+    reviews_ = reviews_.stack()
+    reviews_ = reviews_.reset_index()
+    expanded_reviews = [reviews_.drop(0, axis=1), pd.DataFrame(list(reviews_[0].values))]
+    reviews_df = pd.concat(expanded_reviews, axis=1)
+    reviews_df['rating-date'] = reviews_df['rating-date'].apply(pd.Timestamp)
+    reviews_df['rating'] = reviews_df['rating'].apply(lambda x: float(x.split()[0]))
+    reviews_df.drop('user_location', axis=1, inplace=True)
+    reviews_df['level_1'] = reviews_df['level_1'].apply(lambda x: int(x.split('-')[-1]))
+    reviews_df.columns = ['bus_id', 'review no', 'content', 'rating', 'date', 'user']
+    return reviews_df
+
+def create_photos_df(df):
+    '''
+    creates simple photos df associating photos with a bus id
+    ========
+    PARAMETERS
+    df: pandas df - from create_flattened_dataframe function
+    ========
+    RETURNS
+    new_df: pandas df - dataframe containing reviews
+    '''
+    pass
+
+def create_general_df(df):
+    '''
+    Create general df for buisness Identification and rating.
+    PARAMETERS
+    df: pandas df - from create_flattened_dataframe function
+    ========
+    RETURNS
+    new_df: pandas df - dataframe containing reviews
+    '''
+    pass
+
+
+def save_df_to_json(df, file_location):
+    '''
+    Stores pandas dataframe as single .json file
+    ========
+    PARAMETERS
+    df: pandas dataframe 
+    file_location: string - location of file to save df data
+    ========
+    RETURNS
+    None
+    '''
+    df.to_json(file_location)
+
+def save_file_to_s3(file_location, bucket_name, bucket_key):
     '''
     Stores pandas dataframe as single .json file in save_df_to_s3_json
     ========
     PARAMETERS
-    df: pandas dataframe - data to store in S3 bucket for later analysis
+    file_location: string - location of file to upload to s3
     bucket_name: string - s3 bucket to store .json file
-    file_name: string - desired file name
+    bucket_key: string - desired file name on s3
     ========
     RETURNS
     None
     '''
     aws = boto3.resource('s3')
-
-
-
-    
-
+    ww_all = aws.Bucket(bucket_name)
+    ww_all.upload_file(file_location, bucket_key)
 
 def extract_reviews_df():
     '''
