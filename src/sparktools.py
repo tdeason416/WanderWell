ae32b4a981fb337d8ee5a0ff40bd0992f6303964
tdeason416 <tdeason416@gmail.com>
2017-03-21 10:08:37 -0700

    updated sparktools.py

--- src/sparktools.py
+++ src/sparktools.py
@@ -1,9 +1,3 @@
-'''
-Up here there be docstrings ye best be reading them
-'''
-
-
-
 import re
 import pyspark as ps
 import os 
@@ -14,13 +8,15 @@ from src import build_df
 from pyspark.sql import functions as F
 from pyspark.sql.functions import rand
 from pyspark.ml.feature import HashingTF, IDF, Tokenizer, StopWordsRemover
+from pyspark.ml.classification import NaiveBayes
 # from pyspark.ml.feature import NGram # maybe
 
 
 
 class SparkNLPClassifier(object):
     '''
-    this allows you to classify a spark object
+    Builds trained model in spark based on labeled yelp_academic_dataset, 
+    and evaluates data based on output of the training set.
     '''
     def __init__(self, local_file=False):
         '''
@@ -34,16 +30,30 @@ class SparkNLPClassifier(object):
         TODO
         '''
         self.spark = spark = ps.sql.SparkSession.builder \
-            .appName("yelp_explore") \
+            .appName("reviews_nlp") \
             .getOrCreate() 
         if not local_file:
             train_url = 's3n://wanderwell-ready/yelp_academic_reviews.json'
             self.train = spark.read.json(train_url)
-            self.test_df = None 
         else:
             self.df = spark.read.json(local_file)
+        self.train_popular = None
+        self.train_positive = None
 
-    def split_labeled_sets(self, label, thres=0):
+    def generate_binary_labels(self, df, label, thres=0):
+        '''
+        Converts given label in dataframe into binary value, and drops all other labels
+        '''
+        self.spark.udf.register('imbin', lambda x: 1 if x >= thres else 0)
+        self.spark.udf.register('words_only', self._rem_non_letters)
+        df.registerTempTable('df')
+        r_bin =  self.spark.sql('''
+            SELECT array(words_only(text)) as content, int(imbin({})) as label
+            FROM df
+                    '''.format(label))
+        return r_bin
+
+    def split_labeled_sets(self, df, label):
         '''
         Seperates labeled sets into negative and positive values
         --------
@@ -53,31 +63,33 @@ class SparkNLPClassifier(object):
         --------
         returns: sparkdf -  modifys original self.train dataframe
         '''
-        self.spark.udf.register('imbin', lambda x: 1 if x >= thres else 0)
-        self.spark.udf.register('mkascii', self.rem_non_ascii)
-        train_df = self.train.copy()
-        train_df.registerTempTable('train')
-        r_bin =  self.spark.sql('''
-            SELECT array(mkascii(text)) as content, int(imbin({})) as label
-            FROM train
-                    '''.format(label))
-        mincount_pos = r_bin.filter('label = 1').count()
-        mincount_neg = r_bin.filter('label = 0').count()
+        mincount_pos = df.filter('label = 1').count()
+        mincount_neg = df.filter('label = 0').count()
         if mincount_pos > mincount_neg:
             mincount = mincount_neg
         else:
             mincount = mincount_pos
-        dataset_neg = r_bin.filter('relevant = 0').orderBy(rand()).limit(mincount)
-        dataset_pos = r_bin.filter('relevant = 1').orderBy(rand()).limit(mincount)
-        df_labels = dataset_pos.union(dataset_neg)
-        self.train = df_labels
+        dataset_neg = df.filter('label = 0').orderBy(rand()).limit(mincount)
+        dataset_pos = df.filter('label = 1').orderBy(rand()).limit(mincount)
+        return dataset_pos.union(dataset_neg)
 
+    def train_vectorize(self):
+        '''
+        Applies vectorize to the training set
+        '''
+        self.train_popular = self.generate_binary_labels(self.train, 'useful + funny + cool', 3)
+        self.train_popular = self.split_labeled_sets(self.train_popular, 'useful + funny + cool')
+        self.train_popular = self.vectorize(self.train_popular, 2500)
+        self.train_positive = self.generate_binary_labels(self.train, 'stars', 4)
+        self.train_positive = self.split_labeled_sets(self.train, 'stars')
+        self.train_positive = self.vectorize(self.train_positive, 2500)
 
-    def vectorize(self, n_features=2500):
+    def vectorize(self, df, n_features=2500):
         '''
         generates vectorized features from the self.traindf dataframe
         --------
         Parameters
+        df: spark dataframe - object to be featurized
         n_features: int -  max number of words to be used as features
         --------
         Returns
@@ -85,7 +97,7 @@ class SparkNLPClassifier(object):
         '''
         self.spark.udf.register('listjoin', lambda x: ' '.join(x))
         remover = StopWordsRemover(inputCol="content", outputCol="filtered")
-        df_lab_stopped = remover.transform(self.train)
+        df_lab_stopped = remover.transform(df)
         df_lab_stopped.registerTempTable('df_lab_stopped')
         stop_strings = self.spark.sql('''
                     SELECT listjoin(filtered) as filtered, content, label
@@ -98,15 +110,23 @@ class SparkNLPClassifier(object):
         idf = IDF(inputCol="rawFeatures", outputCol="features")
         idfModel = idf.fit(featurizedData)
         rescaledData = idfModel.transform(featurizedData)
-        self.train = rescaledData
+        return rescaledData
     
-    def train_sentiment(self):
+    def train(self):
         '''
+        
         '''
         pass
 
-
-    def rem_non_ascii(self, text):
+    def _rem_non_letters(self, text):
+        '''
+        removes all non lowercase letters and spaces from string
+        --------
+        Parameters
+        text: string - text to convert
+        --------
+        Returns: ntext.split() - list of lowercase single word strings
+        '''
         lets = []
         ntext = text.lower()
         ntext = re.sub("[^a-z' ]",' ',ntext)
