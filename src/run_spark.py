26a70835aec1fbf09bea1882a84405c50685020a
tdeason416 <tdeason416@gmail.com>
2017-03-23 18:38:20 -0700

    added comit to bucket commands

--- src/run_spark.py
+++ src/run_spark.py
@@ -1,11 +1,10 @@
 import re
 import os               # for environ variables in Part 3
-%load_ext autoreload
-%autoreload 2
 import numpy as np
 import pandas as pd
 from pyspark.sql.functions import rand
 from sparktools import SparkNLPClassifier
+import build_df
 
 
 
@@ -20,10 +19,12 @@ testrf = model.train_test_split()
 model.train_random_forest()
 predictionrf = model.predict(testrf)
 
-rf_pop = predictionrf.select('probability','label').to_pandas
+rf_pop = predictionrf.select('probability','label').toPandas()
 
 
 rf_pop.to_json('../data/rf_model_performance/rf_popular.json')
+
+build_df.save_file_to_s3('../data/rf_model_performance/rf_popular.json', 'wanderwell-ready')
 
 ### TRAINING RANDOM FOREST on Relevant
 model = SparkNLPClassifier()
@@ -34,9 +35,11 @@ testrf = model.train_test_split()
 model.train_random_forest()
 predictionrf = model.predict(testrf)
 
-rf_rel = predictionrf.select('probability','label').to_pandas
+rf_rel = predictionrf.select('probability','label').toPandas()
 
 rf_rel.to_json('../data/rf_model_performance/rf_relevant.json')
+
+build_df.save_file_to_s3('../data/rf_model_performance/rf_relevant.json', 'wanderwell-ready')
 
 ### TRAIN NAIVE BAYES ON relevance
 model = SparkNLPClassifier()
@@ -47,9 +50,11 @@ testrf = model.train_test_split()
 model.train_naive_bayes()
 predictionnb = model.predict(testrf)
 
-nb_rel = predictionnb.select('probability','label').to_pandas
+nb_rel = predictionnb.select('probability','label').toPandas()
 
 nb_rel.to_json('../data/rf_model_performance/nb_relevant.json')
+
+build_df.save_file_to_s3('../data/rf_model_performance/nb_relevant.json', 'wanderwell-ready')
 
 ### TRAIN NAIVE BAYES ON Prefered
 model = SparkNLPClassifier()
@@ -60,6 +65,8 @@ testrf = model.train_test_split()
 model.train_naive_bayes()
 predictionnb = model.predict(testrf)
 
-nb_pop = predictionnb.select('probability','label').to_pandas
+nb_pop = predictionnb.select('probability','label').toPandas()
 
 nb_pop.to_json('../data/rf_model_performance/nb_popular.json')
+
+build_df.save_file_to_s3('../data/rf_model_performance/nb_popular.json', 'wanderwell-ready')
